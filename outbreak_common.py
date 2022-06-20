f97bfc03d172bc6221693d6a4bf73e71a2021215
Emory Hufbauer <mindoftea@gmail.com>
2022-06-20 15:41:31 -0400

    Create outbreak_common.py

---
+++ outbreak_common.py
@@ -0,0 +1,9 @@
+import requests
+
+server = 'api.outbreak.info' # or 'dev.outbreak.info'
+auth = 'Bearer 0ed52bbfb6c79d1fd8e9c6f267f9b6311c885a4c4c6f037d6ab7b3a40d586ad0' # keep this private!
+nopage = 'fetch_all=true&page=0' # worth verifying that this works with newer ES versions as well
+
+def get_outbreak_data(endpoint, argstring, server=server, auth=auth):
+    auth = {'Authorization': str(auth)}
+    return requests.get(f'https://{server}/{endpoint}?q={argstring}', headers=auth)
