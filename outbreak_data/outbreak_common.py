310a955b677a9f6890cc479e4168e280d27a0055
Bryan-Az <36939025+Bryan-Az@users.noreply.github.com>
2022-06-23 14:34:12 -0700

    added visualization demos on new branch

--- outbreak_data/outbreak_common.py
+++ outbreak_data/outbreak_common.py
@@ -1,5 +1,6 @@
 import requests
 import pandas as pd
+import numpy as np
 
 server = 'api.outbreak.info' # or 'dev.outbreak.info'
 auth = ***REMOVED*** # keep this private!
@@ -11,6 +12,22 @@ def get_outbreak_data(endpoint, argstring, server=server, auth=auth):
 
 def cases_by_location(location, server=server, auth=auth):
     raw_data =  get_outbreak_data('covid19/query',
-        f'location_id:{location}&sort=date&fields=date,confirmed_numIncrease&{nopage}',
+        f'location_id:{location}&sort=date&fields=date,confirmed_numIncrease,admin1&{nopage}',
         server, auth )
     return pd.DataFrame(raw_data.json()['hits'])
+
+def get_multiple_locations(locations):
+    out = []
+    for state in locations:
+        data = cases_by_location(state)
+        out.append(data)
+    return pd.concat(out)
+
+def rolling_average(data, num_days):
+    if len(data) <=  num_days:
+        print('Not enough data')
+        return None
+    else:
+        output_rolled = np.convolve(data, np.ones(num_days)/num_days, mode="valid")
+        pads = np.array([0] * (num_days - 1))
+        return np.concatenate((pads, output_rolled))
