e9b0ca620cf5063e6602f8eebf3c7cf18c716400
Bryan-Az <36939025+Bryan-Az@users.noreply.github.com>
2022-06-27 18:22:29 -0700

    fixed bug

--- outbreak_tools/outbreak_tools.py
+++ outbreak_tools/outbreak_tools.py
@@ -2,18 +2,18 @@ import altair as alt
 from outbreak_data import outbreak_data
 
 
-def plot_increase(location, num_pages):
+def plot_increase(location, all_data, num_pages = None):
     """
     Visualizes the confirmed increase in number of cases
     
     Arguments:
         location: Location as a string or list of location strings
         num_pages: Amount of pages (1000 obs/page) of data to use
-        
+        all_data: Whether to use all data or num_pages
     Returns:
         An interactive altair plot
     """
-    data = outbreak_data.page_cases_by_location(location, num_pages)
+    data = outbreak_data.cases_by_location(location, all_data, num_pages)
     # base feature viz // amount of new covid cases
     base = alt.Chart(data).mark_line().encode(
         x='date:T',
