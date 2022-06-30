0979cca2c5665aad4fed6670fd818bbfa061ea37
Bryan-Az <36939025+Bryan-Az@users.noreply.github.com>
2022-06-30 16:42:15 -0700

    + tests & refactored get_data

---
+++ tests/test_outbreak_tools.py
@@ -0,0 +1,17 @@
+import pytest
+from outbreak_tools import outbreak_tools
+import altair as alt
+
+
+def _test_single_location():
+    location = 'USA_US-CA'
+    plot = outbreak_tools.plot_increase(location)
+    assert(type(plot) == alt.vegalite.v4.api.Chart)
+
+
+def test_plot_increase():
+    """
+    Tests the cases_by_location visualization tool in outbreak_tools
+    """
+    _test_single_location()
+
