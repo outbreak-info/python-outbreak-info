e398d9e975b9edae2cf5d28ed0effab303e6ae06
Bryan-Az <36939025+Bryan-Az@users.noreply.github.com>
2022-07-07 14:03:21 -0700

    updated tests to reflect changes in refactor_multiple_locs

--- tests/test_outbreak_data.py
+++ tests/test_outbreak_data.py
@@ -1,12 +1,3 @@
-import pytest
-# Used the request the data from the API
-import requests
-# Used to parse the datetime features in the data
-from datetime import datetime
-# Used to write the urls to call the API with
-from urllib.parse import urljoin
-# Used to introduce delays between tests to avoid overloading the API.
-from time import sleep, time
 from outbreak_data import outbreak_data
 
 
@@ -18,17 +9,26 @@ def _test_get_location():
     location = 'USA_US-CA'
     out = outbreak_data.get_outbreak_data('covid19/query',
                             f'q=location_id:{location}&sort=date&fields=date,confirmed_numIncrease,admin1&{outbreak_data.nopage}')
-    assert(out.admin1.unique()[0] == 'California')
+    assert out.admin1.unique()[0] == 'California', f'get_outbreak_data not returning correct data for {location}'
 
 
 def _test_get_multiple_locations():
     """
     Test the ability of get data to return multiple locations.
     """
-    location = ['USA_US-CA', 'USA_US-NY)']
+    location = ['USA_US-CA', 'USA_US-NY', 'USA_US-TX']
+    locations = '(' + ' OR '.join(location) + ')'
     out = outbreak_data.get_outbreak_data('covid19/query',
-                                          f'q=location_id:{location}&sort=date&fields=date,confirmed_numIncrease,admin1&{outbreak_data.nopage}')
-    assert(len(out.admin1.unique()) == 2)
+                                          f'q=location_id:{locations}'
+                                          f'&sort=date&fields=date,confirmed_numIncrease,admin1&{outbreak_data.nopage}',
+                                          collect_all=True)
+    assert len(out.admin1.unique()) == len(location), f'get_outbreak_data returning incorrect number of locations, ' \
+                                          f'expected {len(location)}'
+    location_names = sorted(['California', 'New York', 'Texas'])
+    output_names = sorted(out.admin1.unique())
+    for i in range(len(location_names)):
+        assert location_names[i] == output_names[i], f'output locations {location_names} do not correspond to input' \
+                                                     f' codes: {location}'
 
 
 def test_get_outbreak_data():
@@ -39,8 +39,28 @@ def test_get_outbreak_data():
     _test_get_multiple_locations()
 
 
+def _test_cases_single_location():
+    location = 'USA_US-CA'
+    out = outbreak_data.cases_by_location(location)
+    assert out.admin1.unique()[0] == 'California', f'cases_by_location not returning correct data for {location}'
+
+
+def _test_cases_multiple_location():
+    location = ['USA_US-CA', 'USA_US-NY', 'USA_US-TX']
+    locations = '(' + ' OR '.join(location) + ')'
+    out = outbreak_data.cases_by_location(locations)
+    assert len(out.admin1.unique()) == len(location), f'cases_by_location returning an incorrect number of locations,' \
+                                          f' expected {len(location)}'
+    location_names = sorted(['California', 'New York', 'Texas'])
+    output_names = sorted(out.admin1.unique())
+    for i in range(len(location_names)):
+        assert location_names[i] == output_names[i], f'output locations {location_names} do not correspond to input' \
+                                                     f' codes: {location}'
+
+
 def test_cases_by_location():
     """
     Main test
     """
-    pass
+    _test_cases_single_location()
+    _test_cases_multiple_location()
