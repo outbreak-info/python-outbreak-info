2603eb64e433655d27e512e99ff223b1dde49201
Bryan-Az <36939025+Bryan-Az@users.noreply.github.com>
2022-07-22 15:02:35 -0700

    modified tests to reflect json output & format of input locations

--- tests/test_outbreak_data.py
+++ tests/test_outbreak_data.py
@@ -1,4 +1,5 @@
 from outbreak_data import outbreak_data
+import pandas as pd
 
 
 def _test_get_location():
@@ -8,8 +9,10 @@ def _test_get_location():
     """
     location = 'USA_US-CA'
     out = outbreak_data.get_outbreak_data('covid19/query',
-                            f'q=location_id:{location}&sort=date&fields=date,confirmed_numIncrease,admin1&{outbreak_data.nopage}')
-    assert out.admin1.unique()[0] == 'California', f'get_outbreak_data not returning correct data for {location}'
+                                          f'q=location_id:{location}&sort=date&fields=date,confirmed_numIncrease,admin1'
+                                          f'&{outbreak_data.nopage}', collect_all=True)
+    df = pd.DataFrame(out['hits'])
+    assert df.admin1.unique()[0] == 'California', f'get_outbreak_data not returning correct data for {location}'
 
 
 def _test_get_multiple_locations():
@@ -22,10 +25,11 @@ def _test_get_multiple_locations():
                                           f'q=location_id:{locations}'
                                           f'&sort=date&fields=date,confirmed_numIncrease,admin1&{outbreak_data.nopage}',
                                           collect_all=True)
-    assert len(out.admin1.unique()) == len(location), f'get_outbreak_data returning incorrect number of locations, ' \
-                                          f'expected {len(location)}'
+    df = pd.DataFrame(out['hits'])
+    assert len(df.admin1.unique()) == len(location), f'get_outbreak_data returning incorrect number of locations, ' \
+                                                      f'expected {len(location)}'
     location_names = sorted(['California', 'New York', 'Texas'])
-    output_names = sorted(out.admin1.unique())
+    output_names = sorted(df.admin1.unique())
     for i in range(len(location_names)):
         assert location_names[i] == output_names[i], f'output locations {location_names} do not correspond to input' \
                                                      f' codes: {location}'
@@ -46,16 +50,15 @@ def _test_cases_single_location():
 
 
 def _test_cases_multiple_location():
-    location = ['USA_US-CA', 'USA_US-NY', 'USA_US-TX']
-    locations = '(' + ' OR '.join(location) + ')'
+    locations = ['USA_US-CA', 'USA_US-NY', 'USA_US-TX']
     out = outbreak_data.cases_by_location(locations)
-    assert len(out.admin1.unique()) == len(location), f'cases_by_location returning an incorrect number of locations,' \
-                                          f' expected {len(location)}'
+    assert len(out.admin1.unique()) == len(locations), f'cases_by_location returning an incorrect number of locations,'\
+                                                       f' expected {len(locations)}'
     location_names = sorted(['California', 'New York', 'Texas'])
     output_names = sorted(out.admin1.unique())
     for i in range(len(location_names)):
         assert location_names[i] == output_names[i], f'output locations {location_names} do not correspond to input' \
-                                                     f' codes: {location}'
+                                                     f' codes: {locations}'
 
 
 def test_cases_by_location():
