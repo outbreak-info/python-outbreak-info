21c135efaaeba1d38776b1b3dedd54007a46e3a8
Sarah Randall <srandall02@icloud.com>
2022-07-23 23:17:09 -0700

    fix

--- tests/test_outbreak_data.py
+++ tests/test_outbreak_data.py
@@ -12,44 +12,44 @@ sys.path.append('/Users/sarahrandall/Python-outbreak-info/outbreak_data')
 from outbreak_data import outbreak_data
 
 
-# def _test_get_location():
-#     """
-#     Test the ability of get data to return a location's case data within a
-#     pandas dataframe
-#     """
-#     location = 'USA_US-CA'
-#     out = outbreak_data.get_outbreak_data('covid19/query',
-#                             f'q=location_id:{location}&sort=date&fields=date,confirmed_numIncrease,admin1&{outbreak_data.nopage}')
-#     assert(out.admin1.unique()[0] == 'California')
+def _test_get_location():
+    """
+    Test the ability of get data to return a location's case data within a
+    pandas dataframe
+    """
+    location = 'USA_US-CA'
+    out = outbreak_data.get_outbreak_data('covid19/query',
+                            f'q=location_id:{location}&sort=date&fields=date,confirmed_numIncrease,admin1&{outbreak_data.nopage}')
+    assert(out.admin1.unique()[0] == 'California')
 
 
-# def _test_get_multiple_locations():
-#     """
-#     Test the ability of get data to return multiple locations.
-#     """
-#     location = '(USA_US-CA OR USA_US-NY)'
-#     out = outbreak_data.get_outbreak_data('covid19/query',
-#                                           f'q=location_id:{location}&sort=date&fields=date,confirmed_numIncrease,admin1&{outbreak_data.nopage}')
-#     assert(len(out.admin1.unique()) == 2)
+def _test_get_multiple_locations():
+    """
+    Test the ability of get data to return multiple locations.
+    """
+    location = '(USA_US-CA OR USA_US-NY)'
+    out = outbreak_data.get_outbreak_data('covid19/query',
+                                          f'q=location_id:{location}&sort=date&fields=date,confirmed_numIncrease,admin1&{outbreak_data.nopage}')
+    assert(len(out.admin1.unique()) == 2)
 
 
-# def _test_page_data():
-#     pass
+def _test_page_data():
+    pass
 
 
-# def test_get_outbreak_data():
-#     """
-#     Main test
-#     """
-#     _test_get_location()
-#     _test_get_multiple_locations()
+def test_get_outbreak_data():
+    """
+    Main test
+    """
+    _test_get_location()
+    _test_get_multiple_locations()
 
 
-# def test_cases_by_location():
-#     """
-#     Main test
-#     """
-#     pass
+def test_cases_by_location():
+    """
+    Main test
+    """
+    pass
 
 class Test_Lineage_Mutations:
     
