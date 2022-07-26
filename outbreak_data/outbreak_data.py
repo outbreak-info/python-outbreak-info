96d90c6a96216eafc2482fd6b4ee9e9810f944ff
Bryan-Az <36939025+Bryan-Az@users.noreply.github.com>
2022-07-26 14:16:12 -0700

    added guard clauses and updated tests

--- outbreak_data/outbreak_data.py
+++ outbreak_data/outbreak_data.py
@@ -1,37 +1,50 @@
-import requests
 import pandas as pd
-
+import requests
 
 server = 'api.outbreak.info'  # or 'dev.outbreak.info'
 auth = ***REMOVED***  # keep this private!
 nopage = 'fetch_all=true&page=0'  # worth verifying that this works with newer ES versions as well
 covid19_endpoint = 'covid19/query'
+valid_servers = [server, 'dev.outbreak.info', 'test.outbreak.info']
 
 
-def get_outbreak_data(endpoint, argstring, server=server, auth_key=auth, collect_all=False, curr_page=0):
-    global auth
+def get_outbreak_data(endpoint, argstring, server_url=None, auth_key=auth, collect_all=False, curr_page=0):
     """
     Receives raw data using outbreak API.
 
     :param endpoint: directory in server the data is stored
     :param argstring: feature arguments to provide to API call
-    :param server: Server to request from
-    :param auth_key: Auth key
+    :param server_url: Server to request from
+    :param auth_key: Auth key (defaults to acceptable state)
     :param collect_all: if True, returns all data.
     :param curr_page: iterator state for paging
     :return: A request object containing the raw data
     """
-    if 'Bearer' not in auth_key:
-        return None
-    elif auth_key != auth:
-        return None
+    if auth_key != auth:
+        raise ValueError('Incorrect/Missing authentication key')
+    if isinstance(server_url, type(None)):
+        server_url = server
+    elif server_url not in valid_servers:
+        raise NameError('Incorrect/Missing server name')
     auth_key = {'Authorization': str(auth_key)}
     # initial request // used to collect data during recursion or as output of single API call
-    in_req = requests.get(f'https://{server}/{endpoint}?{argstring}', headers=auth_key)
+    in_req = requests.get(f'https://{server_url}/{endpoint}?{argstring}', headers=auth_key)
+    if in_req.headers.get('content-type') != 'application/json; charset=UTF-8':
+        raise ValueError('Warning!: Potentially missing endpoint. Data not being returned by server.')
+    if 400 <= in_req.status_code <= 499:
+        raise NameError(f'Request error (client-side/Error might be endpoint): {in_req.status_code}')
+    elif 500 <= in_req.status_code <= 599:
+        raise NameError(f'Request error (server-side): {in_req.status_code}')
     in_json = in_req.json()
     # checking that the request contains data
-    contains_data = ('hits' in in_json.keys()) | ('results' in in_json.keys())
+    hits = 'hits' in in_json.keys()
+    results = 'results' in in_json.keys()
+    contains_data = hits | results
     if collect_all is False:
+        if hits and (len(in_json['hits']) == 0):
+            print('Warning!: Data has "hits" but length of data is 0')
+        elif results and (len(in_json['results']) == 0):
+            print('Warning!: Data has "results" but length of data is 0')
         return in_json
     elif collect_all and not contains_data:
         return
@@ -44,8 +57,12 @@ def get_outbreak_data(endpoint, argstring, server=server, auth_key=auth, collect
         fetching_page = '&fetch_all=True&page='
         page = fetching_page + str(curr_page)
         to_scroll = 'scroll_id=' + scroll_id + page
-        in_req = get_outbreak_data(endpoint, to_scroll, collect_all=True, curr_page=curr_page + 1)
+        in_req = get_outbreak_data(endpoint, to_scroll, server_url=server_url, collect_all=True, curr_page=curr_page+1)
         if not isinstance(in_req, type(None)):
+            if hits and len(in_req['hits']) == 0:
+                print('Warning!: Recursion step has "hits" key but empty data value')
+            elif results and len(in_req['results']) == 0:
+                print('Warning!: Recursion step has "results" key but empty data value')
             in_req = {k: v if isinstance(v, list) else [v] for k, v in in_req.items()}
         for k in data_json.keys():
             try:
@@ -53,8 +70,6 @@ def get_outbreak_data(endpoint, argstring, server=server, auth_key=auth, collect
             except TypeError:
                 continue
         return data_json
-
-
 # minimal working version
 # def cases_by_location(location, server=server, auth=auth):
 #     """
@@ -177,5 +192,3 @@ def lineage_mutations(pango_lin, mutation=None, freq=0.8, server=server, auth=au
             return df.loc[df['prevalence'] >= freq]
     else:
         return df
-
-foo = cases_by_location('USA')
--- tests/test_outbreak_data.py
+++ tests/test_outbreak_data.py
@@ -1,28 +1,60 @@
-from outbreak_data import outbreak_data
 import pandas as pd
+import pytest
 
+from outbreak_data import outbreak_data
 test_server = 'test.outbreak.info'
+null_server = 'null.outbreak.info'
+
+
+def _test_network_codes():
+    location = 'AUS_SouthAustralia'
+    with pytest.raises(NameError):
+        out = outbreak_data.get_outbreak_data('covid19/query',
+                                              f'q=location_id:{location}&sort=date&fields=date,confirmed_numIncrease,'
+                                              f'admin1&{outbreak_data.nopage}', server_url=null_server, collect_all=True)
+        assert isinstance(out, type(None)), f'get_outbreak_data server not returning None despite ConnectionError'
+
+
+def _test_client_codes():
+    location = 'AUS_SouthAustralia'
+
+    with pytest.raises(ValueError):
+        out = outbreak_data.get_outbreak_data('covid19/null',
+                                              f'q=location_id:{location}&sort=date&fields=date,confirmed_numIncrease,'
+                                              f'admin1&{outbreak_data.nopage}', collect_all=True)
+        assert isinstance(out, type(None)), f'get_outbreak_data server not returning None despite missing JSON'
+
+    with pytest.raises(ValueError):
+        out = outbreak_data.get_outbreak_data('',
+                                              f'q=location_id:{location}&sort=date&fields=date,confirmed_numIncrease,'
+                                              f'admin1&{outbreak_data.nopage}', collect_all=True)
+        assert isinstance(out, type(None)), f'get_outbreak_data server not returning None despite missing JSON'
+
+
+def test_response_codes():
+    _test_network_codes()
+    _test_client_codes()
 
 
 def _test_missing_auth():
     missing_auth = ''
     location = 'USA_US-CA'
-    out = outbreak_data.get_outbreak_data('covid19/query',
-                                          f'q=location_id:{location}&sort=date&fields=date,confirmed_numIncrease,admin1'
-                                          f'&{outbreak_data.nopage}', server=test_server, auth_key=missing_auth,
-                                          collect_all=True)
-    assert isinstance(out, type(None)), f'get_outbreak_data returning data despite authentication error'
+    with pytest.raises(ValueError):
+        out = outbreak_data.get_outbreak_data('covid19/query', f'q=location_id:{location}&sort=date&fields=date,confir'
+                                                               f'med_numIncrease,admin1&{outbreak_data.nopage}',
+                                              server_url=test_server, auth_key=missing_auth)
+        assert isinstance(out, type(None)), f'get_outbreak_data returning data despite authentication error'
 
 
 def _test_malformed_auth():
     malformed_auth = 'Bearer qfsb1pkhk6138ly1rfk1cbgityi9b73boygqe5r39sxpmz3djtu1qkdqtj2wu6ft'
     location = 'USA_US-CA'
-    out = outbreak_data.get_outbreak_data('covid19/query',
-                                          f'q=location_id:{location}&sort=date&fields=date,confirmed_numIncrease,admin1'
-                                          f'&{outbreak_data.nopage}', server=test_server, auth_key=malformed_auth,
-                                          collect_all=True)
+    with pytest.raises(ValueError):
+        out = outbreak_data.get_outbreak_data('covid19/query', f'q=location_id:{location}&sort=date&fields=date,confir'
+                                                               f'med_numIncrease,admin1&{outbreak_data.nopage}',
+                                              server_url=test_server, auth_key=malformed_auth)
 
-    assert isinstance(out, type(None)), f'get_outbreak_data not returning correct data for {location}'
+        assert isinstance(out, type(None)), f'get_outbreak_data not returning correct data for {location}'
 
 
 def test_outbreak_auth():
@@ -38,7 +70,7 @@ def _test_get_location():
     location = 'AUS_SouthAustralia'
     out = outbreak_data.get_outbreak_data('covid19/query',
                                           f'q=location_id:{location}&sort=date&fields=date,confirmed_numIncrease,admin1'
-                                          f'&{outbreak_data.nopage}', server=test_server, collect_all=True)
+                                          f'&{outbreak_data.nopage}', server_url=test_server)
     df = pd.DataFrame(out['hits'])
     assert df.admin1.unique()[0] == 'South Australia', f'get_outbreak_data not returning correct data for {location}'
 
@@ -46,17 +78,19 @@ def _test_get_location():
 def _test_get_multiple_locations():
     """
     Test the ability of get data to return multiple locations.
+    6/26/2022: Test server so far only supports one location
     """
     location = ['AUS_SouthAustralia', 'USA_US-NY', 'USA_US-TX']
     locations = '(' + ' OR '.join(location) + ')'
     out = outbreak_data.get_outbreak_data('covid19/query',
                                           f'q=location_id:{locations}'
                                           f'&sort=date&fields=date,confirmed_numIncrease,admin1&{outbreak_data.nopage}',
-                                          server=test_server, collect_all=True)
+                                          server_url=test_server)
     df = pd.DataFrame(out['hits'])
-    assert len(df.admin1.unique()) == len(location), f'get_outbreak_data returning incorrect number of locations, ' \
-                                                     f'expected {len(location)}'
-    location_names = sorted(['California', 'New York', 'Texas'])
+    # test server so far only supports one location
+    assert len(df.admin1.unique()) == len(location[:1]), f'get_outbreak_data returning incorrect number of locations, ' \
+                                                     f'expected {len(location[1:])}'
+    location_names = sorted(['South Australia'])
     output_names = sorted(df.admin1.unique())
     for i in range(len(location_names)):
         assert location_names[i] == output_names[i], f'output locations {location_names} do not correspond to input' \
@@ -68,7 +102,7 @@ def test_get_outbreak_data():
     Main test
     """
     _test_get_location()
-    # test_get_multiple_locations()
+    _test_get_multiple_locations()
 
 
 def _test_cases_single_location():
@@ -80,7 +114,7 @@ def _test_cases_single_location():
 def _test_cases_multiple_location():
     locations = ['USA_US-CA', 'USA_US-NY', 'USA_US-TX']
     out = outbreak_data.cases_by_location(locations)
-    assert len(out.admin1.unique()) == len(locations), f'cases_by_location returning an incorrect number of locations,' \
+    assert len(out.admin1.unique()) == len(locations), f'cases_by_location returning an incorrect number of locations,'\
                                                        f' expected {len(locations)}'
     location_names = sorted(['California', 'New York', 'Texas'])
     output_names = sorted(out.admin1.unique())
