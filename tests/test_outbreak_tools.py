345640977feafabd7f805b43cb795d2a1b1f6599
Bryan-Az <36939025+Bryan-Az@users.noreply.github.com>
2022-08-04 16:14:47 -0700

    added guard clauses & changed lookup verification method

--- tests/test_outbreak_tools.py
+++ tests/test_outbreak_tools.py
@@ -4,13 +4,23 @@ import altair as alt
 
 def _test_single_location():
     location = 'USA_US-CA'
-    plot = outbreak_tools.plot_increase(location)
-    assert(type(plot) == alt.vegalite.v4.api.Chart)
+    expected_loc_name = 'California'
+    plot = outbreak_tools.plot_case_increase(location)
+    assert type(plot) == alt.vegalite.v4.api.Chart, 'Visualization not returning correct format'
+    plotted_loc = plot.data.admin1.unique()[0]
+    assert plotted_loc == expected_loc_name, 'Visualization not plotting correct location'
 
+def _test_multiple_locations():
+    locations = ['USA_US-CA', 'USA_US-OR', 'USA_US-WA', 'USA_US-NV']
+    plot = outbreak_tools.plot_case_increase(locations)
+    assert type(plot) == alt.vegalite.v4.api.Chart, 'Visualization not returning correct format'
+    plotted_locs = plot.data.admin1.unique()
+    assert len(plotted_locs) == len(locations), 'Visualization not plotting all locations'
 
-def test_plot_increase():
+def test_plot_case_increase():
     """
     Tests the cases_by_location visualization tool in outbreak_tools
     """
     _test_single_location()
+    _test_multiple_locations()
 
