445c63ec830c0345d626d4c81c84ce487c2ea033
Sarah Randall <srandall02@icloud.com>
2022-11-29 01:07:49 -0800

    n/a

--- outbreak_data/authenicate_user.py
+++ outbreak_data/authenicate_user.py
@@ -14,6 +14,49 @@ import requests
 
 OUTBREAK_INFO_AUTH = "https://api.outbreak.info/genomics/get-auth-token"
 
+
+def printTerms():
+    """	TERMS OF USE for R Package and
+	
+	    Reminder of GISAID's Database Access Agreement
+	
+	Your ability to access and use Data in GISAID, including your access and
+	use of same via R Package, is subject to the terms and conditions of
+	GISAID's Database Access Agreement (“DAA”) (which you agreed to
+	when you requested access credentials to GISAID), as well as the
+	following terms:
+	
+	1. You will treat all data contained in the R Package consistent with
+	other Data in GISAID and in accordance with GISAID's Database Access
+	Agreement;
+	
+	2. You will not distribute, or re-distribute Data made available through
+	GISAID to any third party other than Authorized Users as contemplated by
+	the DAA;
+	
+	3. USE OF R PACKAGE: Any visualizations, charts, graphs,
+	graphics, pictographs, plots, or other displays you create via the R
+	Package may be exclusively used for academic and research purposes.
+	No other types of uses are allowed;
+	
+	4. Any use of visualizations, charts, graphs, graphics, pictographs,
+	plots, or other displays created via the R Package in an academic or
+	research publication, including in a paper, manuscript, preprint, website,
+	web service, or any other media material must be in conformity with the
+	GISAID Publishing Guidelines, available at https://www.gisaid.org/publish,
+	and the DAA, available at https://www.gisaid.org/daa; and
+	
+	5. By using the R Package you reaffirm your understanding of these
+	terms and the DAA.
+	
+	When using this data, please state, \"This data was obtained from GISAID via the outbreak.info API\". 
+	WE DO NOT SUPPORT THIRD PARTY APPLICATIONS. THIS PACKAGE IS MEANT FOR RESEARCH AND VISUALIZATION PURPOSES ONLY. 
+	If you want to build third party applications, please contact GISAID via https://www.gisaid.org/help/contact/.
+	"""
+    print(printTerms.__doc__)
+
+
+
 def get_authentication():
     """
     Get the authentication token from a 'hidden' file.
@@ -26,9 +69,9 @@ def get_authentication():
           read_token = A.read()
           
     #get token
-    token = read_token
+    global read_token
 
-    return(token)
+    return(read_token)
 
 
 def set_authentication(token):
@@ -51,6 +94,7 @@ def set_authentication(token):
     hidden_file = os.path.join(curr_dir, auth_token_filename)
     if not os.path.isfile(hidden_file):  #check to see if file was saved in directory
         assert (FileNotFoundError)
+        
     get_authentication()
         
         
@@ -64,6 +108,7 @@ def authenticate_user():
 
     #POST request to the OUTBREAK TOKEN API to get a token 
     r = requests.post(OUTBREAK_INFO_AUTH)
+    
     if (r.status_code != 200):
         print("Could not get authentication-token")
         r.raise_for_status()
@@ -91,11 +136,10 @@ def authenticate_user():
         
         # response = code authenticates something
         
-        
-    # if (response.status_code == 200):
-    #     print("\nAuthenticated successfully!\n")
-    #     # printTerms()
-    #     # authToken = response$headers$`x-auth-token`
+    if (get_authentication())
+          print("\nAuthenticated successfully!\n")
+          printTerms()
+          authToken = read_token
     #     if(authToken == None):
     #       setAuthToken(authToken)
      
