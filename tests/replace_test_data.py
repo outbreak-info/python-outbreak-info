bc8fac1f1e07300fec91e73ae8a2ca970c9a05e2
Sarah Randall <srandall02@icloud.com>
2023-03-27 21:25:21 -0700

    fixed tests

--- tests/replace_test_data.py
+++ tests/replace_test_data.py
@@ -15,23 +15,8 @@ null_server = 'null.outbreak.info'
 
 def update_all_test_lineage_mutations():
     #test one
-    save_file = os.path.join(os.path.dirname(__file__), 'test_data', 'test_one.csv')
-    val1 =  outbreak_data.lineage_mutations('BA.2', server=test_server)
-    val1.to_csv(save_file)
-
-    #test two
-    save_file = os.path.join(os.path.dirname(__file__), 'test_data', 'test_two.csv')
-    val1 = outbreak_data.lineage_mutations('BA.2 OR B.1.1.7', server=test_server)
-    val1.to_csv(save_file)
-
-    #test three
-    save_file = os.path.join(os.path.dirname(__file__), 'test_data', 'test_three.csv')
-    val1 = outbreak_data.lineage_mutations('BA.2, B.1.1.7', server=test_server)
-    val1.to_csv(save_file)
-
-    #test four 
-    save_file = os.path.join(os.path.dirname(__file__), 'test_data', 'test_four.csv')
-    val1 = outbreak_data.lineage_mutations('BA.2','s:p681h', server=test_server)
+    save_file = os.path.join(os.path.dirname(__file__), 'test_data', 'lineage_mutations.csv')
+    val1 =  outbreak_data.lineage_mutations('BA.2 OR B.1.1.7', server=test_server)
     val1.to_csv(save_file)
 
 def update_mutations_by_lineage():
@@ -44,45 +29,97 @@ def update_global_prevalence():
     val1 = outbreak_data.global_prevalence('ba.2', 'orf1a:t842i', server=test_server)
     val1.to_csv(save_file)
 
+def update_all_sequence_counts():
+    #test one
+    save_file = os.path.join(os.path.dirname(__file__), 'test_data', 'sequence_counts.csv')
+    val1 =  outbreak_data.sequence_counts('USA_US-CA', server=test_server)
+    val1.to_csv(save_file)
 
 def update_all_prevalence_by_location():
     #test one
-    save_file = os.path.join(os.path.dirname(__file__), 'test_data', 'prev1.csv')
-    val1 = outbreak_data.prevalence_by_location('USA_US-CO', server=test_server)
-    val1.to_csv(save_file)
-
-    #test two
-    save_file = os.path.join(os.path.dirname(__file__), 'test_data', 'prev2.csv')
+    save_file = os.path.join(os.path.dirname(__file__), 'test_data', 'prevalence_by_location.csv')
     val1 = outbreak_data.prevalence_by_location('USA_US-NY', startswith='b.1', server=test_server)
     val1.to_csv(save_file)
 
-def update_all_sequence_counts():
+def update_all_daily_prev():
     #test one
-    save_file = os.path.join(os.path.dirname(__file__), 'test_data', 'seq_count1.csv')
-    val1 = outbreak_data.sequence_counts(server=test_server)
+    save_file = os.path.join(os.path.dirname(__file__), 'test_data', 'daily_prev.csv')
+    val1 = outbreak_data.daily_prev('BA.2', server=test_server)
     val1.to_csv(save_file)
 
-    #test two
-    save_file = os.path.join(os.path.dirname(__file__),'test_data', 'seq_count2.csv')
-    val1 = outbreak_data.sequence_counts('USA_US-CA', server=test_server)
+def update_all_lineage_by_sub_admin():
+    #test one
+    save_file = os.path.join(os.path.dirname(__file__), 'test_data', 'sub_admin.csv')
+    val1 = outbreak_data.lineage_by_sub_admin('XBB.1.5', location='USA', server=test_server)
+    val1.to_csv(save_file)
+    
+def update_all_collection_date():
+    #test one
+    save_file = os.path.join(os.path.dirname(__file__), 'test_data', 'collection_date.csv')
+    val1 = outbreak_data.collection_date('BA.1', server=test_server)
+    val1.to_csv(save_file)
+    
+def update_all_submission_date():
+    #test one
+    save_file = os.path.join(os.path.dirname(__file__), 'test_data', 'submission_date.csv')
+    val1 = outbreak_data.submission_date('BA.1', server=test_server)
+    val1.to_csv(save_file)
+    
+def update_all_mutation_details():
+    #test one
+    save_file = os.path.join(os.path.dirname(__file__), 'test_data', 'mutation_details.csv')
+    val1 = outbreak_data.mutation_details('orf1a:t842i', server=test_server)
+    val1.to_csv(save_file)
+    
+def update_all_daily_lag():
+    #test one
+    save_file = os.path.join(os.path.dirname(__file__), 'test_data', 'daily_lag.csv')
+    val1 = outbreak_data.daily_lag('LUX', server=test_server)
     val1.to_csv(save_file)
 
-    #test three
-    save_file = os.path.join(os.path.dirname(__file__), 'test_data', 'seq_count3.csv')
-    val1 = outbreak_data.sequence_counts('USA', cumulative=True, server=test_server)
+def update_all_wildcard_lineage():
+    #test one
+    save_file = os.path.join(os.path.dirname(__file__), 'test_data', 'wildcard_lineage.csv')
+    val1 = outbreak_data.wildcard_lineage('b.1*', server=test_server)
+    val1.to_csv(save_file)
+    
+def update_all_wildcard_location():
+    #test one
+    save_file = os.path.join(os.path.dirname(__file__), 'test_data', 'wildcard_location.csv')
+    val1 = outbreak_data.wildcard_location('united*', server=test_server)
+    val1.to_csv(save_file)
+    
+def update_all_location_details():
+    #test one
+    save_file = os.path.join(os.path.dirname(__file__), 'test_data', 'location_details.csv')
+    val1 = outbreak_data.location_details('CHN', server=test_server)
+    val1.to_csv(save_file)
+    
+def update_all_wildcard_mutations():
+    #test one
+    save_file = os.path.join(os.path.dirname(__file__), 'test_data', 'wildcard_mutations.csv')
+    val1 = outbreak_data.wildcard_mutations('s:e484*', server=test_server)
     val1.to_csv(save_file)
 
-    #test four
-    save_file = os.path.join(os.path.dirname(__file__), 'test_data', 'seq_count4.csv')
-    val1 = outbreak_data.sequence_counts('USA', True, True, server=test_server)
-    val1.to_csv(save_file)
 
 def main():
-    update_global_prevalence()
-    update_mutations_by_lineage()
     update_all_test_lineage_mutations()
-    update_all_prevalence_by_location()
+    update_mutations_by_lineage()
+    update_global_prevalence()
     update_all_sequence_counts()
+    update_all_prevalence_by_location()
+    update_all_daily_prev()
+    update_all_lineage_by_sub_admin()
+    update_all_collection_date()
+    update_all_submission_date()
+    update_all_mutation_details()
+    update_all_daily_lag()
+    update_all_wildcard_lineage()
+    update_all_wildcard_location()
+    update_all_location_details()
+    update_all_wildcard_mutations()
+    
+    
 
 
 if __name__ == "__main__":
