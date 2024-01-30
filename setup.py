e1f46664120ee2d7683850c28734a77223524027
Josh Levy <joshua.levy16@gmail.com>
2024-01-29 17:09:06 -0800

    Update setup.py
    update version number

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ description = ("Python interface to Outbreak.info,"
 
 setup(
     name="python_outbreak_info",
-    version="1.0",
+    version="1.0.4",
     package_dir={"": "src"},
     packages=find_packages(where="src"),
     author="Outbreak.info dev team",
